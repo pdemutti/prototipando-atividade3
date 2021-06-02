4c3db3ad2a20d08f3c928169d45a7a908d4cd79d
Lucas Moura <lucasmoura.dev@gmail.com>
2021-06-02 20:49:50 -0300

    [FEAT] - Desktop fix position

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const swiper = new Swiper('.swiper-container', {
+  
+  const swiper = new Swiper('.swiper-container', {
+    
     // Optional parameters
     direction: 'horizontal',
-    loop: true,
+    // loop: true,
   
     // If we need pagination
     pagination: {
@@ -13,9 +15,40 @@ const swiper = new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    centerInsufficientSlides: true,
+       // Default parameters   
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+   // Responsive breakpoints   
+   breakpoints: {  
+  
+      // when window width is <= 480px     
+      480: {       
+         slidesPerView: 1,       
+         spaceBetween: 20     
+      },   
+  
+      // when window width is <= 640px     
+      640: {       
+         slidesPerView: 2,       
+         spaceBetween: 30     
+      },
+
+      // when window width is <= 640px     
+      1200: {       
+        slidesPerView: 2,        
+     },
+  
+   } 
   });
+
+  document.querySelector('.float__button').addEventListener('click', () => {
+    document.querySelector('.hero__logo').scrollIntoView({
+      behavior: 'smooth'
+    });
+  })
+
+
+
+
+
+
